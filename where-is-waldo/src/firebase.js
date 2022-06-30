df4848be091e021ccc40a08a99d9efd35bb4a766
Houser97 <arturo.riverar97@gmail.com>
2022-06-30 18:05:36 -0500

    Set up firebase

---
+++ where-is-waldo/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDueasQBc04KnrvtlkWhOf1SIqphpD0Jfo",
+  authDomain: "where-is-waldo-cee83.firebaseapp.com",
+  projectId: "where-is-waldo-cee83",
+  storageBucket: "where-is-waldo-cee83.appspot.com",
+  messagingSenderId: "501165901982",
+  appId: "1:501165901982:web:6d3b339286fbd4c0cf938f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
