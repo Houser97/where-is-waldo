4b8923a039dafe8ee84c48b2285ed4572fa8c785
Houser97 <arturo.riverar97@gmail.com>
2022-06-30 14:13:44 -0500

    Place target's photos and names

--- where-is-waldo/src/components/header.js
+++ where-is-waldo/src/components/header.js
@@ -1,5 +1,8 @@
 import '../styles/header.css';
 import logo from '../images/Adventure-time-logo.png';
+import BMO from '../images/BMO.jpeg';
+import Marceline from '../images/marceline.png'
+import Trunks from '../images/Trunks.jpeg'
 import Timer from './timer';
 
 const Navbar = () => {
@@ -9,18 +12,18 @@ const Navbar = () => {
                 <div className='where-is'>Where is...?</div>
                 <div className='characters'>
                     <div className='photo-name'>
-                        <div className='fin-photo character'></div>
-                        <div className='name'>Finn</div>
+                        <img src={BMO} alt='BMO' className='first-photo character'></img>
+                        <div className='name'>BMO</div>
                     </div>
 
                     <div className='photo-name'>
-                        <div className='second-photo character'></div>
-                        <div className='name'>Finn</div>
+                        <img src={Marceline} alt='Marceline' className='first-photo character'></img>
+                        <div className='name'>Marceline</div>
                     </div>
 
                     <div className='photo-name'>
-                        <div className='third-photo character'></div>
-                        <div className='name'>Finn</div>
+                        <img src={Trunks} alt='Tree Trunks' className='first-photo character'></img>
+                        <div className='name'>Tree Trunks</div>
                     </div>
                 </div>
             </div>
